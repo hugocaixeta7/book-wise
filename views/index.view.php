fb4a1945d17ecf44eff943734b9c860916a68914
Hugo Caixeta <hugoccaixeta3@gmail.com>
2025-08-19 13:29:12 -0300

    fix: routes

--- views/index.view.php
+++ views/index.view.php
@@ -13,7 +13,7 @@
         <div class="flex">
             <div class="w-1/3">Imagem</div>
             <div class="space-y-1">
-                <a href="livro?id=<?= $livro['id'] ?>" class="font-semibold hover:underline"><?= $livro['titulo'] ?></a>
+                <a href="index.php/livro?id=<?= $livro['id'] ?>" class="font-semibold hover:underline"><?= $livro['titulo'] ?></a>
                 <div class="text-xs italic"><?= $livro['autor'] ?></div>
                 <div class="text-xs italic">⭐⭐⭐⭐⭐(3 Avaliações)</div>
             </div>
