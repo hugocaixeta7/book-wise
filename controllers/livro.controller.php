39d1f989f23f086f1f16db509cdb2ac84f4a75dc
Hugo Caixeta <hugoccaixeta3@gmail.com>
2025-08-20 14:34:29 -0300

    Refatorando classe DB

--- controllers/livro.controller.php
+++ controllers/livro.controller.php
@@ -1,5 +1,7 @@
 <?php
 
-$livro = (new DB)->livro($_REQUEST)['id'];
+$livro = (new DB)
+    ->query("select * from livros where id = :id", Livro::class, ['id' => $_GET['id']])
+    ->fetch();
 
 view('livro', compact('livro'));
--- database.php
+++ database.php
@@ -13,24 +13,15 @@ class DB
         $this->db = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $user, $pass);
     }
     #endregion
-    /**
-     * Retorna todos os livros do banco de dados
-     *
-     * @return array[Livro]
-     */
-    public function livros($pesquisa = '') {
-        $prepare = $this->db->prepare("select * from livros where usuario_id = 1 and titulo like :pesquisa");
-        $prepare->bindValue(':pesquisa', "%$pesquisa%");
-        $prepare->setFetchMode(PDO::FETCH_CLASS, Livro::class);
-        $prepare->execute();        
-        return $prepare->fetchAll();
-    }
 
-    public function livro($id) {
-        $prepare = $this->db->prepare("select * from livros where id = :id");
-        $prepare->bindValue('id', $id);
-        $prepare->setFetchMode(PDO::FETCH_CLASS, Livro::class);
-        $prepare->execute();        
-        return $prepare->fetch();
+    public function query($query, $class = null, $params = [])
+    {
+        $prepare = $this->db->prepare($query);
+        if ($class) {
+            $prepare->setFetchMode(PDO::FETCH_CLASS, $class);
+        }
+        $prepare->execute($params);
+
+        return $prepare;
     }
 }
